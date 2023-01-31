0621d9c9d3876f707773a9dc9ed8940651fb11a5
Mohsin Khan Niazi <mohsinniazi818839@gmail.com>
2023-02-01 00:12:26 +0500

    structuring

---
+++ calculator_appp/lib/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'components/my_button.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          MyButton(),
+        ],
+      )),
+    );
+  }
+}
